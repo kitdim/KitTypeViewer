da2bbcc8031028139464a26bed3f2e03726a213b
kitdim <kitaykin9808@gmail.com>
2022-09-30 11:33:10 +0300

    0.0.1

--- KitTypeViewer/Program.cs
+++ KitTypeViewer/Program.cs
@@ -1,1 +1,92 @@
-﻿
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+Console.WriteLine("***** KitDimViewer *****");
+string typeName = "";
+
+do
+{
+    Console.WriteLine("\nВведите имя типа");
+    Console.Write("или нажмите на Q для завершения:");
+
+    typeName = Console.ReadLine();
+    if (typeName.Equals("Q", StringComparison.OrdinalIgnoreCase)) break;
+
+    try
+    {
+        var t = Type.GetType(typeName);
+        Console.WriteLine();
+        ListVariousStats(t);
+        ListFields(t);
+        ListProp(t);
+        ListMethods(t);
+        ListInterfaces(t);
+    }
+    catch
+    {
+        Console.WriteLine("Указанный тип не найден.");
+    }
+} while (true);
+
+
+static void ListMethods(Type t)
+{
+    Console.WriteLine("***** Методы *****");
+
+    var methodNames = from n in t.GetMethods() select n.Name;
+    foreach (var name in methodNames)
+    {
+        Console.WriteLine("->{0}", name);
+    }
+    Console.WriteLine();
+}
+
+static void ListFields(Type t)
+{
+    Console.WriteLine("***** Поля *****");
+
+    var fieldNames = from f in t.GetFields() select f.Name;
+    foreach (var name in fieldNames)
+    {
+        Console.WriteLine("->{0}", name);
+    }
+    Console.WriteLine();
+}
+
+static void ListProp(Type t)
+{
+    Console.WriteLine("***** Свойства *****");
+
+    var propNames = from p in t.GetProperties() select p.Name;
+    foreach (var name in propNames)
+    {
+        Console.WriteLine("->{0}", name);
+    }
+    Console.WriteLine();
+}
+
+static void ListInterfaces(Type t)
+{
+    Console.WriteLine("***** Интерфейсы *****");
+
+    var ifaces = from i in t.GetInterfaces() select i;
+    foreach (var name in ifaces)
+    {
+        Console.WriteLine("->{0}", name);
+    }
+    Console.WriteLine();
+}
+
+static void ListVariousStats(Type t)
+{
+    Console.WriteLine("***** Различная статистика *****");
+
+    Console.WriteLine("Базовый класс: {0}", t.BaseType);
+    Console.WriteLine("Указанный тип является абстактным? {0}", t.IsAbstract);
+    Console.WriteLine("Указанный тип является запечатанным? {0}", t.IsSealed);
+    Console.WriteLine("Указанный тип является обобщенным? {0}", t.IsGenericTypeDefinition);
+    Console.WriteLine("'Указанный тип является классом? {0}", t.IsClass);
+
+    Console.WriteLine();
+}
